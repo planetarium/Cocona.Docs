a6fca0ba991ac5a9e2f7a8faf166f0ee229fe8ae
moreal <dev.moreal@gmail.com>
2022-06-22 11:34:48 +0900

    style: apply editorconfig

--- Cocona.Docs/DocumentCommand.cs
+++ Cocona.Docs/DocumentCommand.cs
@@ -1,109 +1,113 @@
 ﻿using System.Collections.Immutable;
-using System.Runtime.InteropServices;
 using System.Text;
 using Cocona.Command;
 
 namespace Cocona.Docs;
+
 public class DocumentCommand
 {
-    [PrimaryCommand]
-    public void Generate([FromService] ICoconaCommandProvider commandProvider, [Argument(Name = "OUT_DIR", Description = "Path to output directory.")] string outDir)
-    {
-        var collection = commandProvider.GetCommandCollection();
-        var prefix = AppDomain.CurrentDomain.FriendlyName;
-        var rendered = GenerateDocs(collection, prefix: prefix, root: true);
-        const string css = @"<style>dd {
+  [PrimaryCommand]
+  public void Generate([FromService] ICoconaCommandProvider commandProvider,
+    [Argument(Name = "OUT_DIR", Description = "Path to output directory.")] string outDir)
+  {
+    var collection = commandProvider.GetCommandCollection();
+    var prefix = AppDomain.CurrentDomain.FriendlyName;
+    var rendered = GenerateDocs(collection, prefix: prefix, root: true);
+    const string css = @"<style>dd {
     margin-bottom: 2em;
 }</style>";
-        foreach (KeyValuePair<string, string> pair in rendered)
-        {
-            
-            File.WriteAllText(Path.Combine(outDir, pair.Key == "" ? "index" : pair.Key), css + pair.Value);
-        }
-    }
-
-    private ImmutableDictionary<string, string> GenerateDocs(CommandCollection commandCollection, string prefix = "", bool root = false)
+    foreach (KeyValuePair<string, string> pair in rendered)
     {
-        var subcommandsDictionary = ImmutableDictionary<string, string>.Empty;
-        StringBuilder stringBuilder = new StringBuilder();
-        foreach (var command in commandCollection.All)
-        {
-            if (!command.IsPrimaryCommand && command.SubCommands is CommandCollection { })
-            {
-                subcommandsDictionary = subcommandsDictionary
-                    .AddRange(GenerateDocs(command.SubCommands, prefix + " " + command.Name));
-                continue;
-            }
-
-            stringBuilder.Append($"<h2>{prefix + (root || commandCollection.All.Count == 1 ? "" :  " " + command.Name)}</h2>");
-            stringBuilder.Append($"<p>{command.Description}</p>");
-
-            if (command.Options.Count > 0)
-            {
-                stringBuilder.Append("<h3>Options</h3>");
-                stringBuilder.Append("<dl>");
-                foreach (var option in command.Options)
-                {
-                    string defaultValueString = option.DefaultValue.HasValue ? $" {option.DefaultValue}" : "";
-                    stringBuilder.Append($"<dt>");
-
-                    if (option.ShortName.Count > 0)
-                    {
-                        foreach (char shortName in option.ShortName)
-                        {
-                            stringBuilder.Append($"<code>-{shortName}</code>");
-                        }
-
-                        stringBuilder.Append(", ");
-                    }
-
-                    stringBuilder.Append($"<code>--{option.Name} {ResolveTypeToString(option.OptionType)}{defaultValueString}</code></dt>");
-                    stringBuilder.Append($"<dd>{option.Description}</dd>");
-                }
-
-                stringBuilder.Append("</dl>");
-            }
-            
-            if (command.Arguments.Count > 0)
-            {
-                stringBuilder.Append("<h3>Arguments</h3>");
-                stringBuilder.Append("<dl>");
-                foreach (var argument in command.Arguments)
-                {
-                    string defaultValueString = argument.DefaultValue.HasValue ? $" {argument.DefaultValue}" : "";
-                    stringBuilder.Append($"<dt><code>{argument.Name}</code> {ResolveTypeToString(argument.ArgumentType)}{defaultValueString}</dt>");
-                    stringBuilder.Append($"<dd>{argument.Description}</dd>");
-                }
-                stringBuilder.Append("</dl>");
-            }
-        }
-
-        if (subcommandsDictionary.Count > 0)
-        {
-            stringBuilder.Append("<h2>Subcommands</h2>");
-            stringBuilder.Append("<ul>");
-            foreach (KeyValuePair<string, string> pair in subcommandsDictionary)
-            {
-                stringBuilder.Append($"<li><a href=\"./{pair.Key}\">{pair.Key}</a></li>");
-            }
-
-            stringBuilder.Append("</ul>");
-        }
-        
-        subcommandsDictionary = subcommandsDictionary.Add(prefix, stringBuilder.ToString());
-
-        return subcommandsDictionary;
+      File.WriteAllText(Path.Combine(outDir, pair.Key == "" ? "index" : pair.Key), css + pair.Value);
     }
+  }
 
-    private string ResolveTypeToString(Type type)
+  private ImmutableDictionary<string, string> GenerateDocs(CommandCollection commandCollection, string prefix = "",
+    bool root = false)
+  {
+    var subcommandsDictionary = ImmutableDictionary<string, string>.Empty;
+    StringBuilder stringBuilder = new StringBuilder();
+    foreach (var command in commandCollection.All)
     {
-        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+      if (!command.IsPrimaryCommand && command.SubCommands is CommandCollection { })
+      {
+        subcommandsDictionary = subcommandsDictionary
+          .AddRange(GenerateDocs(command.SubCommands, prefix + " " + command.Name));
+        continue;
+      }
+
+      stringBuilder.Append($"<h2>{prefix + (root || commandCollection.All.Count == 1 ? "" : " " + command.Name)}</h2>");
+      stringBuilder.Append($"<p>{command.Description}</p>");
+
+      if (command.Options.Count > 0)
+      {
+        stringBuilder.Append("<h3>Options</h3>");
+        stringBuilder.Append("<dl>");
+        foreach (var option in command.Options)
         {
-            return type.GetGenericArguments()[0].Name + "?";
+          string defaultValueString = option.DefaultValue.HasValue ? $" {option.DefaultValue}" : "";
+          stringBuilder.Append($"<dt>");
+
+          if (option.ShortName.Count > 0)
+          {
+            foreach (char shortName in option.ShortName)
+            {
+              stringBuilder.Append($"<code>-{shortName}</code>");
+            }
+
+            stringBuilder.Append(", ");
+          }
+
+          stringBuilder.Append(
+            $"<code>--{option.Name} {ResolveTypeToString(option.OptionType)}{defaultValueString}</code></dt>");
+          stringBuilder.Append($"<dd>{option.Description}</dd>");
         }
-        else
+
+        stringBuilder.Append("</dl>");
+      }
+
+      if (command.Arguments.Count > 0)
+      {
+        stringBuilder.Append("<h3>Arguments</h3>");
+        stringBuilder.Append("<dl>");
+        foreach (var argument in command.Arguments)
         {
-            return type.Name;
+          string defaultValueString = argument.DefaultValue.HasValue ? $" {argument.DefaultValue}" : "";
+          stringBuilder.Append(
+            $"<dt><code>{argument.Name}</code> {ResolveTypeToString(argument.ArgumentType)}{defaultValueString}</dt>");
+          stringBuilder.Append($"<dd>{argument.Description}</dd>");
         }
+
+        stringBuilder.Append("</dl>");
+      }
     }
+
+    if (subcommandsDictionary.Count > 0)
+    {
+      stringBuilder.Append("<h2>Subcommands</h2>");
+      stringBuilder.Append("<ul>");
+      foreach (KeyValuePair<string, string> pair in subcommandsDictionary)
+      {
+        stringBuilder.Append($"<li><a href=\"./{pair.Key}\">{pair.Key}</a></li>");
+      }
+
+      stringBuilder.Append("</ul>");
+    }
+
+    subcommandsDictionary = subcommandsDictionary.Add(prefix, stringBuilder.ToString());
+
+    return subcommandsDictionary;
+  }
+
+  private string ResolveTypeToString(Type type)
+  {
+    if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+    {
+      return type.GetGenericArguments()[0].Name + "?";
+    }
+    else
+    {
+      return type.Name;
+    }
+  }
 }

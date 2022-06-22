969d7912b4b253a64f66e706e79b3df00d9908df
moreal <dev.moreal@gmail.com>
2022-06-22 17:38:09 +0900

    docs: introduce example project

---
+++ samples/Cocona.Docs.Examples.Calc/Program.cs
@@ -0,0 +1,21 @@
+﻿using Cocona;
+
+[HasSubCommands(typeof(Cocona.Docs.DocumentCommand), "docs")]
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    CoconaApp.Run<Program>(args);
+  }
+
+  [Command]
+  public void Add(
+    [Argument(Name = "X", Description = "The first operand.")]
+    int x,
+    [Argument(Name = "Y", Description = "The second operand.")]
+    int y
+  )
+  {
+    Console.Out.WriteLine(x + y);
+  }
+}

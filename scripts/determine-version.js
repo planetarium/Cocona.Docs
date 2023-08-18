c617b8f9d4acd03dbc74e8463fa33d495041b9e8
XxshiftxX <qjfrntop12@gmail.com>
2023-08-18 15:40:47 +0900

    cli: change console.log into error

--- scripts/determine-version.js
+++ scripts/determine-version.js
@@ -79,7 +79,7 @@ const getPackageVersion = async () => {
 const main = async () => {
   const packageVersion = await getPackageVersion();
   
-  console.log('packageVersion', packageVersion);
+  console.error('packageVersion', packageVersion);
 
   if (!process.env.GITHUB_OUTPUT) return;
 
